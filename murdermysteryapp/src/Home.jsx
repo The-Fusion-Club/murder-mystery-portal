f2038220f4f750ecfcb5f2a02f9b73277089c835
Manomay Kagalkar <manomay111@gmail.com>
2023-02-08 14:46:46 +0530

    TnC modal ready

--- murdermysteryapp/src/Home.jsx
+++ murdermysteryapp/src/Home.jsx
@@ -1,11 +1,13 @@
 import logo from './images/logo.png'
 import advitya from './images/advitya.png'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Home = () => {
 
     // hooks
     const navigate = useNavigate()
+    const [modalView, setModalView] = useState(sessionStorage.getItem('modalView') || 'flex')
 
     // event handlers
     const handleInfoClick = () => {
@@ -18,6 +20,11 @@ const Home = () => {
 
     const handleRulesClick = () => {
         navigate('/rules')
+    }
+
+    const handleHideModal = () => {
+        setModalView('none')
+        sessionStorage.setItem('modalView', 'none')
     }
 
     return(
@@ -44,6 +51,18 @@ const Home = () => {
                     </button>
                 </section>
             </header>
+            <section id='modal' className='modal layer' style={{ display: modalView }}>
+                <section className='modal box'>
+                    <h1>By visiting this site, you agree to the following:</h1>
+                    <h1>-> You shall read and follow the <a style={{color: 'aqua'}} href="/rules">rules of the event</a></h1>
+                    <h1>-> You will not give spoilers or hints to other teams</h1>
+                    <h1>-> You will not try to attack the system in any way</h1>
+                    <button className='hide button' onClick={handleHideModal} style={{alignSelf: 'center', backgroundColor: 'chartreuse'}}>
+                        ACCEPT
+                        <img style={{maxWidth: '20px'}} src="https://cdn-icons-png.flaticon.com/512/1549/1549612.png" alt="arrow icon" />
+                    </button>
+                </section>
+            </section>
             <footer>
                 made with ♡ by mank
             </footer>
